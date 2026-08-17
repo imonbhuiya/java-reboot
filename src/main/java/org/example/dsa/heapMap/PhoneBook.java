dec8a3d673dcbeb9156d3da914fd8ff0f0d0147d
imonbhuiya <imon.cse15@gmail.com>
2026-08-17 21:40:42 +0200

    Make phone book using HashMap

---
+++ src/main/java/org/example/dsa/heapMap/PhoneBook.java
@@ -0,0 +1,26 @@
+package org.example.dsa.heapMap;
+
+import java.util.HashMap;
+
+public class PhoneBook {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> phoneBook = new HashMap<>();
+
+        phoneBook.put("Imon", "+491919191992");
+        phoneBook.put("Rahim", "+491234566766");
+        phoneBook.put("Karim", "+492327373664");
+
+        System.out.println(phoneBook);
+
+        System.out.println(phoneBook.get("Imon"));
+
+        System.out.println(phoneBook.containsKey("Alice"));
+
+        phoneBook.remove("Karim");
+
+        System.out.println(phoneBook);
+
+    }
+}
