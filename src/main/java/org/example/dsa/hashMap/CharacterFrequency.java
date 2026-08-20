6856a56d54a05140d4c0ee656e525a41e0567b8c
imonbhuiya <imon.cse15@gmail.com>
2026-08-20 23:29:12 +0200

    write code for word frequency check

---
+++ src/main/java/org/example/dsa/hashMap/CharacterFrequency.java
@@ -0,0 +1,26 @@
+package org.example.dsa.hashMap;
+import java.util.HashMap;
+
+public class CharacterFrequency {
+    public static void main(String[] args) {
+
+        HashMap<Character, Integer> counts = new HashMap<>();
+        String word = "programming";
+
+        for( int i = 0; i< word.length(); i++){
+            char character = word.charAt(i);
+            if(counts.containsKey(character))
+            {
+                int oldCount = counts.get(character);
+                int newCount = oldCount + 1;
+                counts.put(character,newCount);
+            }
+            else {
+                counts.put(character,1);
+            }
+
+        }
+        System.out.println(counts);
+    }
+}
+
