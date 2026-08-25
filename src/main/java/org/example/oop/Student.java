33595e85b4ef133f350c6df7dd8de52971fad3f3
imonbhuiya <imon.cse15@gmail.com>
2026-08-26 00:55:14 +0200

    Create class,method,field and constuctor

---
+++ src/main/java/org/example/oop/Student.java
@@ -0,0 +1,54 @@
+package org.example.oop;
+
+
+public class Student {
+
+    //Fields
+     String name;
+     int age;
+     String department;
+
+
+     Student(){
+
+     }
+
+     Student(String name){
+         this.name = name;
+
+     }
+
+
+     //Constructor
+    public Student(String name, int age, String department) {
+        
+        this.name = name;
+        this.age = age;
+        this.department = department;
+        
+    }
+
+    
+
+    //Method
+
+    void instroduce(){
+        System.out.println(
+                "My name is " + name +
+                        ", I am " + age +
+                        " years old my department is " + department
+        );
+    }
+
+
+  //Main function
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Alex" );
+        Student s3 = new Student("Sara" ,22,"IT");
+        s1.instroduce();
+        s2.instroduce();
+        s3.instroduce();
+    }
+
+}
