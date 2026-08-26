6e9185236389897b0e34171f67c3f12107e26f21
imonbhuiya <imon.cse15@gmail.com>
2026-08-26 17:30:49 +0200

    Parent and Child class using Inheritance

---
+++ src/main/java/org/example/oop/Inheritance/Animal.java
@@ -0,0 +1,19 @@
+package org.example.oop.Inheritance;
+
+
+public class Animal {
+
+    void eat(){
+        System.out.println("Eating");
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+    }
+
+
+}
+
---
+++ src/main/java/org/example/oop/Inheritance/Dog.java
@@ -0,0 +1,7 @@
+package org.example.oop.Inheritance;
+
+public class Dog extends Animal{
+    void bark(){
+        System.out.println("Barking");
+    }
+}
